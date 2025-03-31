32bbef7b18c98540827f900fca08202f8f20c8a3
isholadolapo121 <ishola.dolapo121@gmail.com>
2025-03-31 08:27:15 -0700

    Add files via upload
    This is my 3mtt project assessment

---
+++ My Profile Ishola/index.go
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Profile</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, intial-scale=1.0">
+<title>Black Backgrud</title>
+<style>
+body {
+	background-color: black;
+	color: white; /* Ensures text is readable */
+}
+
+</nav>
+</style>
+</head>
+<body>
+<header>
+<nav>
+<u1>
+<li>Portfolio<li>
+<li>About Me<li>
+<li>Hobbies: Work Out, Reading, Football <li>
+
+<li>Contact<li>
+
+<style>
+li {
+	font-weight: bold;
+}
+</style>
+<h1>Ishola Dolapo Ridwan</h1>
+</ui>
+</header>
+<secton class="bio">
+<p>Hi, I'm</p>
+<p>Ishola Dolapo Ridwan</p>
+<p>A young Nigerian with experience in airline ticketing and a software developer. My journey in tech has been exciting and full of learning. I am part of the 3MTT Cohort 3 program, where I continue to sharpen my skills. I am ready to work in a great company and collaborate with a team to build the future together. Looking ahead, my goal is to establish a tech company that will drive innovation and promote Nigeria and Africa on the global stage.</p>
+<img src="profile.jpg" alt="my profile picture" width="150" height="150">
+<footer>
+<h2>Contact</h2>
+<p>Email: ishola.dolapo121@gmail.com</p>
+<p>Phone Number: 09090461205</p>
+
+
+
+</body>
+</html>
